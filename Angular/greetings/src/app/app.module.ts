e34e94623f12eac72b3ebe50112338f3a5b5d430
Abhi Agarwal <abhi_agarwal@dell.com>
2021-04-17 14:16:26 +0530

    Initial commit

---
+++ Angular/greetings/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloWorldComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  entryComponents:[HelloWorldComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+  constructor(private injector: Injector){
+  }
+
+  ngDoBootstrap() {
+    const myCustomElement = createCustomElement(HelloWorldComponent, { injector: this.injector });
+    customElements.define('app-hello-world', myCustomElement);
+  }
+}
